53ced7eeed1e96c86335263070238e133388c158
karthik <chkarthik853@gmail.com>
2025-05-27 15:17:00 +0530

    pycharm work commit

--- stringverification.py
+++ stringverification.py
@@ -1,16 +1,27 @@
-s=['[','{','(']
-g="]})"
-g1="[]{}()"
-c=0
-flag=False
-for i in g1:
-        if(i in s):
-            c=g.find(i)
-            if(i in g):
-                d=g.find(i)
-                if c<d:
-                    flag=True
-                else:break
-
-print(flag)
+class Solution(object):
+    def isValid(self, string):
+        """
+        :type string: str
+        :rtype: bool
+        """
+        s = ['[', '{', '(']
+        g = [']', '}', ')']
+        stack = []
+        flag = True
+        for i in string:
+            if i in s:
+                stack.append(i)
+            elif i in g:
+                if not stack:
+                    flag = False
+                    break
+                expected = s[g.index(i)]
+                if stack[-1] == expected:
+                    stack.pop()
+                else:
+                    flag = False
+                    break
+        if stack:
+            flag = False
+        return flag
 
