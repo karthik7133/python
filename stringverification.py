36850bad18fbbb5e8f32272487067446c880660d
karthik <chkarthik853@gmail.com>
2025-05-27 15:19:19 +0530

    pycharm work commit

--- stringverification.py
+++ stringverification.py
@@ -1,27 +1,22 @@
-class Solution(object):
-    def isValid(self, string):
-        """
-        :type string: str
-        :rtype: bool
-        """
-        s = ['[', '{', '(']
-        g = [']', '}', ')']
-        stack = []
-        flag = True
-        for i in string:
-            if i in s:
-                stack.append(i)
-            elif i in g:
-                if not stack:
-                    flag = False
-                    break
-                expected = s[g.index(i)]
-                if stack[-1] == expected:
-                    stack.pop()
-                else:
-                    flag = False
-                    break
-        if stack:
+s = ['[', '{', '(']
+g = [']', '}', ')']
+string="{}("
+stack = []
+flag = True
+for i in string:
+    if i in s:
+        stack.append(i)
+    elif i in g:
+        if not stack:
             flag = False
-        return flag
+            break
+        expected = s[g.index(i)]
+        if stack[-1] == expected:
+            stack.pop()
+        else:
+            flag = False
+            break
+if stack:
+    flag = False
+print(flag)
 
