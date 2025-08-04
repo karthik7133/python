68d495c28ff653bf7e3864fb11abc17463c47317
karthik <chkarthik853@gmail.com>
2025-08-04 12:21:55 +0530

    pycharm work commit

--- missingnum.py
+++ missingnum.py
@@ -1,3 +1,17 @@
-a=[1,3,2];
+a=[7,1,5,3,6,4]
 a.sort()
-print(a)
+min=min(a)
+dif=0
+minindex=a.index(min)
+if(minindex==0):
+    print(max(a)-min)
+
+elif(minindex== len(a)-1):
+    print(0)
+else:
+    for i in range(0,len(a)):
+        for j in range(i,len(a)):
+            if dif<(a[j]-a[i]):
+                dif=a[j]-a[i]
+
+    print(dif)
