ebadd6b7e2ba1699aa6129e1f0796f7f8a17cb92
karthik <chkarthik853@gmail.com>
2025-08-04 12:58:59 +0530

    pycharm work commit

--- missingnum.py
+++ missingnum.py
@@ -1,17 +1,12 @@
-a=[7,1,5,3,6,4]
-a.sort()
-min=min(a)
-dif=0
-minindex=a.index(min)
-if(minindex==0):
-    print(max(a)-min)
 
-elif(minindex== len(a)-1):
-    print(0)
-else:
-    for i in range(0,len(a)):
-        for j in range(i,len(a)):
-            if dif<(a[j]-a[i]):
-                dif=a[j]-a[i]
+a = [7, 1, 5, 3, 6, 4]
+max_profit = 0
+min_price = float('inf')
 
-    print(dif)
+for price in a:
+    if price < min_price:
+        min_price = price
+    elif price - min_price > max_profit:
+        max_profit = price - min_price
+
+print(max_profit)
